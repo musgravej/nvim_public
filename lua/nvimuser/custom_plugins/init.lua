554c1f608076e02e762a463ed0d7e13fdcac20a5
musgravej <jim.musgrave@imtapps.com>
2024-10-24 15:25:11 -0500

    update init for custom plugins

--- lua/nvimuser/custom_plugins/init.lua
+++ lua/nvimuser/custom_plugins/init.lua
@@ -3,9 +3,9 @@ local function load_lua_files(directory)
 	for _, file in ipairs(vim.fn.globpath(directory, "*.lua", 0, 1)) do
 		local module_name = file:match(".*/(.*).lua$")
 		if module_name and module_name ~= "init" then -- Avoid loading self
-			require("/jmusgrave/custom_plugins." .. module_name) -- Correct module path
+			require("/nvimuser/custom_plugins." .. module_name) -- Correct module path
 		end
 	end
 end
 
-load_lua_files(vim.fn.stdpath("config") .. "/lua/jmusgrave/custom_plugins/")
+load_lua_files(vim.fn.stdpath("config") .. "/lua/nvimuser/custom_plugins/")
