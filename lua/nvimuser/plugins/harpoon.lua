29ebc66db0c50592bd6f21801e3db048e5c45bd0
musgravej <jim.musgrave@imtapps.com>
2024-10-23 22:56:23 -0500

    update harpoon

--- lua/nvimuser/plugins/harpoon.lua
+++ lua/nvimuser/plugins/harpoon.lua
@@ -1,6 +1,7 @@
 return {
-    "theprimeagen/harpoon",
+    "ThePrimeagen/harpoon",
     dependencies = { "nvim-lua/plenary.nvim" },
+    branch = "harpoon2",
     config = function()
       require("harpoon"):setup()
     end,
