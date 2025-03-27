6ad583460a24ccb5ca6623187b82601dc02e369b
musgravej <jim.musgrave@imtapps.com>
2025-03-27 10:02:05 -0500

    remove plugins/archived

--- lua/nvimuser/plugins/archived/none-ls.lua
+++
@@ -1,9 +0,0 @@
--- plugins/none-ls.lua
-return {
-    "nvimtools/none-ls.nvim",
-    ft = {"python"},
-    -- opts = function ()
-    --     require "nvimuser.plugins.configs.null-ls"
-    -- end,
-}
-
