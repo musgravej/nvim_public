a10d50b47769b40c21f09ca2be75bc2fba2a5f57
musgravej <jim.musgrave@gmail.com>
2025-10-17 23:31:12 -0500

    update alpha

--- lua/nvimuser/plugins/alpha.lua
+++ lua/nvimuser/plugins/alpha.lua
@@ -1,19 +1,27 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
 
     -- Set header
     dashboard.section.header.val = {
+      -- "",
+      -- " ██████╗  █████╗ ███████╗██╗  ██╗██████╗  ██████╗  █████╗ ██████╗ ██████╗  ",
+      -- " ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██╔══██╗ ",
+      -- " ██║  ██║███████║███████╗███████║██████╔╝██║   ██║███████║██████╔╝██║  ██║ ",
+      -- " ██║  ██║██╔══██║╚════██║██╔══██║██╔══██╗██║   ██║██╔══██║██╔══██╗██║  ██║ ",
+      -- " ██████╔╝██║  ██║███████║██║  ██║██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝ ",
+      -- " ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ",
+      -- "",
       "",
-      " ██████╗  █████╗ ███████╗██╗  ██╗██████╗  ██████╗  █████╗ ██████╗ ██████╗  ",
-      " ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██╔══██╗ ",
-      " ██║  ██║███████║███████╗███████║██████╔╝██║   ██║███████║██████╔╝██║  ██║ ",
-      " ██║  ██║██╔══██║╚════██║██╔══██║██╔══██╗██║   ██║██╔══██║██╔══██╗██║  ██║ ",
-      " ██████╔╝██║  ██║███████║██║  ██║██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝ ",
-      " ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ",
+      " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗     ██████╗    ██╗ ██████╗ ",
+      " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║    ██╔═████╗  ███║██╔═████╗ ",
+      " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║    ██║██╔██║  ╚██║██║██╔██║ ",
+      " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║    ████╔╝██║   ██║████╔╝██║ ",
+      " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║    ╚██████╔╝██╗██║╚██████╔╝ ",
+      " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝     ╚═════╝ ╚═╝╚═╝ ╚═════╝ ",
       "",
       -- "                                                     ",
       -- "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
@@ -44,22 +52,22 @@ return {
       -- [[ `''                                                                      ``' ]],
     }
 
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("<leader> ee", "  > Toggle file explorer", "<cmd>Neotree reveal<CR>"),
-      dashboard.button("<leader> ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("<leader> fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("Session Restore Last", "󰁯  > Restore Last Session", "<cmd>SessionLoadLast<cr>"),
-      dashboard.button("Session List", "󰁯  > Restore Session From List", "<cmd>SessionSelect<cr>"),
-      dashboard.button("Session Manage", "󰁯  > Manage Sessions", "<cmd>Telescope persisted<cr>"),
-      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-    }
+        -- Set menu
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+            dashboard.button("<leader> ee", "  > Toggle file explorer", "<cmd>Neotree reveal<CR>"),
+            dashboard.button("<leader> ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+            dashboard.button("<leader> fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+            dashboard.button("Session Restore Last", "󰁯  > Restore Last Session", "<cmd>SessionLoadLast<cr>"),
+            dashboard.button("Session List", "󰁯  > Restore Session From List", "<cmd>SessionSelect<cr>"),
+            dashboard.button("Session Manage", "󰁯  > Manage Sessions", "<cmd>Telescope persisted<cr>"),
+            dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+        }
 
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
+        -- Send config to alpha
+        alpha.setup(dashboard.opts)
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
 }
